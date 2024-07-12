03a8b783edb4e75561bd7b9416066799806ae0f4
Ch-Sai-Jaswanth <saijaswanthch2003@gmail.com>
2024-07-12 13:01:35 +0530

    Added Emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useRef } from "react";
 import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 import violetbg from "../assets/violetbg.png";
+import Swal from "sweetalert2";
 import "../../src/index.css";
 
 const Contact = () => {
@@ -25,6 +27,47 @@ const Contact = () => {
     });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+  
+  emailjs.send(
+    import.meta.env.EMAILJS_SERVICE_ID, 
+    import.meta.env.EMAILJS_TEMPLATE_ID,
+    {
+      from_name: "CH Sai Jaswanth",
+      to_name : form.name,
+      from_email: "saijaswanthch2003@gmail.com",
+      to_email : form.email,
+      message: "Thank you for contacting us. We will get back to you soon."
+    },
+    import.meta.env.EMAILJS_PUBLIC_KEY
+  )
+  .then(() => {
+    setLoading(false);
+    Swal.fire({
+      icon: "success",
+      title: "Message Sent",
+      text: "Your message has been sent successfully.",
+    });
+
+    setForm({
+      name: "",
+      email: "",
+      message: "",
+    });
+  },
+  (error) => {
+    setLoading(false);
+    console.log(error);
+    Swal.fire({
+      icon: "error",
+      title: "Message Not Sent",
+      text: "Your message has not been sent. Please try again later.",
+    });
+  });
+}
+
   return (
     <div
       className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
@@ -36,7 +79,7 @@ const Contact = () => {
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact</h3>
 
-        <form ref={formRef} className="mt-12 flex flex-col gap-8">
+        <form ref={formRef} className="mt-12 flex flex-col gap-8" onSubmit={handleSubmit}>
           <label className="flex flex-col">
             <span className="text-white font-medium mb-4">Your Name</span>
             <input
@@ -74,13 +117,6 @@ const Contact = () => {
           <button
             type="submit"
             className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
-            onClick={(e) => {
-              e.preventDefault();
-              setLoading(true);
-              setTimeout(() => {
-                setLoading(false);
-              }, 2000);
-            }}
           >
             {loading ? "Sending..." : "Send"}
           </button>
